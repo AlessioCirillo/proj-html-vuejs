dc66b6ff500a0fff0c3c307a9ef2984b7d9e42e3
alessio <alessio.cirillo.1995@gmail.com>
2020-12-02 13:20:43 +0100

    layout completed

--- js/main.js
+++ js/main.js
@@ -14,6 +14,37 @@ const app = new Vue({
             'fab fa-facebook-f',
             'fab fa-instagram',
             'fab fa-linkedin'
-        ]
+        ],
+        contacts:[
+            '382 NE 191st ST # 87394 Miami, FL 33179-3899',
+            '+1(305) 547-9909 (9am - 5pm EST, Monday - Friday)',
+            'support@mxcoach.com'
+        ],
+        explore:[
+            'start here',
+            'blog',
+            'about us',
+            'success story',
+            'courses',
+            'contact us'
+        ],
+        information:[
+            'membership',
+            'purchase guide',
+            'privacy policy',
+            'terms of service'
+        ],
+        footerIcons:[
+            'fab fa-facebook-square',
+            'fab fa-twitter',
+            'fab fa-instagram',
+            'fab fa-linkedin'
+        ],
+        copy:{
+            copyIcon:[
+                'far fa-copyright'
+            ],
+            copyText: '2020 Maxcoach. All Rights Reserved'
+        }
     }
 });
